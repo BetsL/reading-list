000e5ed15683d1ce88b51a254150663b5d74d4f0
Bets <elizabeth.lidstrom@gmail.com>
2019-04-19 21:45:51 -0400

    toggle, button events added; style in gray, lilac

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
+
+    // create element
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+
+    // add button/delete feature w/each new list item ...
+
+    // create element
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode(" - "));
+    // append button to list
+    li.appendChild(button);
+    // for every button created
+    button.onclick = removeParent;
+
 }
 
 function addListAfterClick() {
@@ -25,6 +39,15 @@ function addListAfterKeypress(event) {
     }
 }
 
-button.addEventListener("click", addListAfterClick);
+function removeParent(event) {
+    event.target.parentNode.remove();
+}
 
+ul.onclick=function(event){
+	var target=event.target;
+	target.classList.toggle("done");
+}
+
+
+button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
